52b73fad3c535c1e19855a5ac87b6140433d545b
LeoVal1 <nwadikechibuzo@gmail.com>
2022-07-27 20:36:21 -0500

    main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os # importing operating system module
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 # to stop caching static file
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
